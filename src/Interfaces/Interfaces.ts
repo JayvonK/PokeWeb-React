6bebec009b247f32b7eb6d87d72c84a1bacaf2de
JayvonK <jaykhuth@gmail.com>
2024-03-16 08:02:42 -0700

    more interface

--- src/Interfaces/Interfaces.ts
+++ src/Interfaces/Interfaces.ts
@@ -1,18 +1,43 @@
 interface IPokemonData {
-    abilities: IAbilityObject[]
-    location_area_encounters_URL: string
-    moves: IMovesObject[]
+  abilities: IAbilityObject[];
+  location_area_encounters_URL: string;
+  moves: IMovesObject[];
+  species: {
+    name: string;
+  };
+  sprites: ISpritesObject;
 }
 
 interface IAbilityObject {
-        ability: {
-            name: string
-        }
+  ability: {
+    name: string;
+  };
 }
 
 interface IMovesObject {
-        move: {
-            name: string
-        }
+  move: {
+    name: string;
+  };
 }
-export{}
+
+interface ITypesObject {
+  type: {
+    name: string;
+  };
+}
+
+interface ISpritesObject {
+  front_default: string;
+  front_shiny: string;
+  version: {
+    "generation-v": {
+      "black-white": {
+        animated: {
+          front_default: string;
+        };
+      };
+    };
+  };
+}
+
+export {};
