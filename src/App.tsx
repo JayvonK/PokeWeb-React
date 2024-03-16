8aa2ac123da1bf102b87f170f95ea965d4e59f17
JayvonK <jaykhuth@gmail.com>
2024-03-16 06:47:58 -0700

    basic setup and modal test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
+'use client';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Modal } from 'flowbite-react';
+import { useState } from 'react';
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setOpenModal(true)}>Toggle modal</button>
+      <Modal show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal.Header>Terms of Service</Modal.Header>
+        <Modal.Body>
+          <div className="space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+              companies around the world are updating their terms of service agreements to comply.
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
+              to ensure a common set of data rights in the European Union. It requires organizations to notify users as
+              soon as possible of high-risk data breaches that could personally affect them.
+            </p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button onClick={() => setOpenModal(false)}>I accept</button>
+          <button color="gray" onClick={() => setOpenModal(false)}>
+            Decline
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
